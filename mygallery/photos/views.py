e32d0b75fe2dc738a421c5881117c0c455609864
DenisMuga <denismugah5@gmail.com>
2022-05-29 22:52:58 +0300

    Creating a condition for addPhoto function in views py

--- mygallery/photos/views.py
+++ mygallery/photos/views.py
@@ -16,5 +16,9 @@ def viewPhoto(request, pk):
 def addPhoto(request):
     categories = Category.objects.all()
     
+    if request.method = 'POST':
+        data = request.POST
+        image = request.files.get('image')
+    
     context = {'categories': categories}
     return render(request, 'photos/add.html', context)
